7c6c86debd778fc0fcbe5d194f4a8d1c9eb4ab09
Nguyen Tan Dat <dat.nguye_bk@hcmut.edu.vn>
2021-11-03 04:50:43 +0000

    add multipy

---
+++ week2/log.m
@@ -0,0 +1,11 @@
+N=10
+That took 0.022534 seconds
+N=100
+That took 0.057953 seconds
+N=1000
+That took 36.603218 seconds
+N=10000
+That took 11341.462432 seconds
+
+That MPI work better than the code before I used
+Both experient are work on Docker
